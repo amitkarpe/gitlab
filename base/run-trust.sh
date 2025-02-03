54713a2c483b5a34f9f050df5b4564b9daaca9d0
Amit Karpe <amitkarpe@gmail.com>
2025-02-03 19:26:53 +0800

    added build script

--- base/run-trust.sh
+++ base/run-trust.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/l5q9p9n6
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 590184139818.dkr.ecr.ap-southeast-1.amazonaws.com
 docker build -t lifebit/base .
-docker tag lifebit/base:latest public.ecr.aws/l5q9p9n6/lifebit/base:latest
-docker push public.ecr.aws/l5q9p9n6/lifebit/base:latest
+docker tag lifebit/base:latest 590184139818.dkr.ecr.ap-southeast-1.amazonaws.com/lifebit/base:latest
+docker push 590184139818.dkr.ecr.ap-southeast-1.amazonaws.com/lifebit/base:latest
